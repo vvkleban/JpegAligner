33750c745aadf320786e9d865a784cbbc73986fb
Volodymyr Kleban <admin@vkleban.com>
2017-04-01 22:29:48 -0400

    Figuring out the library work and Exif

--- jpeg_aligner.pl
+++ jpeg_aligner.pl
@@ -1,3 +1,15 @@
 #!/usr/bin/perl
 
+use Image::MetaData::JPEG;
+
+use strict;
+
+my $image = new Image::MetaData::JPEG('/media/raid5/pictures/2017-03-26 New Orleans/P1060114.JPG');
+die 'Error: ' . Image::MetaData::JPEG::Error() unless $image;
+
+my $textual_exif= $image->get_Exif_data('IMAGE_DATA');
+
+use Data::Dumper;
+
+print Dumper $textual_exif;
 
