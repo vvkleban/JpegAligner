f19f3065b0e076f140484a129e1b2ccaad27c4e6
Volodymyr Kleban <admin@vkleban.com>
2017-04-02 15:14:19 -0400

    First working version

--- jpeg_aligner.pl
+++ jpeg_aligner.pl
@@ -3,26 +3,33 @@
 use Image::MetaData::JPEG;
 use Time::Local;
 use String::Escape qw( printable unprintable );
+use POSIX 'strftime';
 use Data::Dumper;
 
 use strict;
 
+# By how many seconds the camera differs from real time
 my %camera_shift_seconds_map = (
-  'samsung_SM-G900H' => 0
+  'samsung_SM-G900H' => 0,
+  'Apple_iPhone_4' => 0,
+  'Canon_Canon_EOS_DIGITAL_REBEL_XSi' => 6956,
+  'LGE_Nexus_4' => 0,
+  'OnePlus_ONEPLUS_A3000' => 0,
+  'Panasonic_DMC-ZS100' => 4172
 );
 
 #-------------------------------------------------------------------------------
 # print string
 #-------------------------------------------------------------------------------
 sub debug($) {
-  print shift;
+#  print shift;
 }
 
 #-------------------------------------------------------------------------------
 # dump variable
 #-------------------------------------------------------------------------------
 sub debug_dump($) {
-  print Dumper shift;
+#  print Dumper shift;
 }
 
 #-------------------------------------------------------------------------------
@@ -48,11 +55,9 @@ sub getEpoch($)
 }
 
 #-------------------------------------------------------------------------------
-# get epoch shift in seconds for this make_model of the camera
-# args:   JPEG object
-# return: seconds to shift the epoch by
+# return: make_model string of the camera that produced given image
 #-------------------------------------------------------------------------------
-sub makeModelShift($)
+sub getMakeModel($)
 {
   my $image = shift;
   my $exif= $image->get_Exif_data('IMAGE_DATA');
@@ -60,9 +65,21 @@ sub makeModelShift($)
   my $model= $exif->{'Model'}[0];
   my $make_model= "${make}_${model}";
   $make_model =~ s/[^[:print:]]//g;
+  $make_model =~ s/[\s]/_/g;
+  return $make_model;
+}
+
+#-------------------------------------------------------------------------------
+# get epoch shift in seconds for this make_model of the camera
+# args:   JPEG object
+# return: number of seconds the camera differs from real time
+#-------------------------------------------------------------------------------
+sub makeModelShift($)
+{
+  my $image = shift;
+  my $make_model = getMakeModel($image);
   if (defined $camera_shift_seconds_map{$make_model})
   {
-    #debug "defined to be " . $camera_shift_seconds_map{$make_model} . " seconds\n";
     return $camera_shift_seconds_map{$make_model};
   }
   else
@@ -76,37 +93,42 @@ sub makeModelShift($)
 #-------------------------------------------------------------------------------
 sub usage()
 {
-  return "Please run: \"perl $0 <path>\" to convert pictures\n";
+  return "Please run: \"perl $0 <source path> <destination path>\" to convert pictures\n";
 }
 
 ################################################################################
 # MAIN
 ################################################################################
 
-if ($#ARGV < 0)
+if ($#ARGV <= 0)
 {
   print STDERR usage();
   exit 1;
 }
 
 my $dir= $ARGV[0];
+my $dest_dir= $ARGV[1];
 
-opendir(my $dh, $dir) || die "Can't opendir $dir: $!";
+opendir(my $dh, $dir) || die "Can't opendir \"$dir:\" for reading: $!";
+opendir(my $dh2, $dest_dir) || die "Can't opendir \"$dest_dir\" for writing: $!";
 my @jpegs = grep { /\.(jpg|jpeg)/i && -f "$dir/$_" } readdir($dh);
 for my $jpeg (@jpegs)
 {
   my $full_path= "$dir/$jpeg";
   my $image= new Image::MetaData::JPEG( $full_path );
+  die 'Error: ' . Image::MetaData::JPEG::Error() unless $image;
   print "Found: \"$jpeg\"\n";
-  print "Its epoch is " . getEpoch($image) . "\n";
-  print "Its shift is " . makeModelShift($image) . "\n";
+  my $claimed_epoch= getEpoch($image);
+  my $recorded_diff= makeModelShift($image);
+  print "Its epoch is " . $claimed_epoch . "\n";
+  print "Its shift is " . $recorded_diff . "\n";
+  my $date_string= strftime('%Y%m%d_%H%M%S', localtime($claimed_epoch - $recorded_diff));
+  print "Calculated date_time: $date_string\n";
+  my $new_name= $date_string . "_" . getMakeModel($image) . ".jpg";
+  print "Renaming \"$jpeg\" into \"$new_name\"\n";
+  link($full_path, "$dest_dir/$new_name");
 }
 closedir $dh;
+closedir $dh2;
 
 
-my $image = new Image::MetaData::JPEG('/media/raid5/pictures/2017-03-26 New Orleans/20170326_124504.jpg');
-die 'Error: ' . Image::MetaData::JPEG::Error() unless $image;
-
-
-print getEpoch($image) . "\n";
-print makeModelShift($image) . "\n";
