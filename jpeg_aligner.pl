c87f858cbcc7edb376221b5d80ee5473fec3ead0
Volodymyr Kleban <admin@vkleban.com>
2017-04-02 01:34:25 -0400

    Started file processing

--- jpeg_aligner.pl
+++ jpeg_aligner.pl
@@ -8,7 +8,7 @@ use Data::Dumper;
 use strict;
 
 my %camera_shift_seconds_map = (
-  'samsung\\00_SM-G900H\\00' => 0
+  'samsung_SM-G900H' => 0
 );
 
 #-------------------------------------------------------------------------------
@@ -58,7 +58,8 @@ sub makeModelShift($)
   my $exif= $image->get_Exif_data('IMAGE_DATA');
   my $make= $exif->{'Make'}[0];
   my $model= $exif->{'Model'}[0];
-  my $make_model= printable("${make}_${model}");
+  my $make_model= "${make}_${model}";
+  $make_model =~ s/[^[:print:]]//g;
   if (defined $camera_shift_seconds_map{$make_model})
   {
     #debug "defined to be " . $camera_shift_seconds_map{$make_model} . " seconds\n";
@@ -70,6 +71,38 @@ sub makeModelShift($)
   }
 
 }
+#-------------------------------------------------------------------------------
+# return: usage string
+#-------------------------------------------------------------------------------
+sub usage()
+{
+  return "Please run: \"perl $0 <path>\" to convert pictures\n";
+}
+
+################################################################################
+# MAIN
+################################################################################
+
+if ($#ARGV < 0)
+{
+  print STDERR usage();
+  exit 1;
+}
+
+my $dir= $ARGV[0];
+
+opendir(my $dh, $dir) || die "Can't opendir $dir: $!";
+my @jpegs = grep { /\.(jpg|jpeg)/i && -f "$dir/$_" } readdir($dh);
+for my $jpeg (@jpegs)
+{
+  my $full_path= "$dir/$jpeg";
+  my $image= new Image::MetaData::JPEG( $full_path );
+  print "Found: \"$jpeg\"\n";
+  print "Its epoch is " . getEpoch($image) . "\n";
+  print "Its shift is " . makeModelShift($image) . "\n";
+}
+closedir $dh;
+
 
 my $image = new Image::MetaData::JPEG('/media/raid5/pictures/2017-03-26 New Orleans/20170326_124504.jpg');
 die 'Error: ' . Image::MetaData::JPEG::Error() unless $image;
